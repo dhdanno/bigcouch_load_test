5cf7da3c7f07421ebd37c70700975e99e37143df
root <root@db01.tor1.voxter.net>
2016-05-10 23:01:42 +0000

    funnies

--- cut_me_off.sh
+++ cut_me_off.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# You've had too much to drink
+# Go home BigCouch, You've had too much to drink
 
 if [ -z "$1" ]; then
   echo "Enter your IP";
@@ -9,5 +9,5 @@ fi
 ME=$1
 # allow me to talk
 iptables -I INPUT 1 -s $ME/32 -d 0.0.0.0/0 -j ACCEPT
-# drop everyone else
+# drop everyone else (cause they aren't your friends)
 iptables -I INPUT 2 -s 0.0.0.0/0 -d 0.0.0.0/0 -j DROP
--- im_fine_now.sh
+++ im_fine_now.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Let me back in, please
 
+# Do this in reverse order or die.
 iptables -D INPUT 2
 iptables -D INPUT 1
