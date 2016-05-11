780c7ba73220f15a5baf0314dcc921b6253d9501
root <root@db01.tor1.voxter.net>
2016-05-11 19:08:02 +0000

    fixed iptables logic

--- cut_me_off.sh
+++ cut_me_off.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Go home BigCouch, You've had too much to drink
 
-#if [ -z "$2" ]; then
-#  echo "Enter your IP and Gateway";
-#  exit;
-#fi
+## IMPORTANT ##
+# You must run this first WITHOUT an argument first and then apply the argument
+# otherwise rules get pushed out
 
-ME=$1
+# Enter the IP you want to allow access from/to
+if [ -n "$1" ]; then
+  echo "Access will be allowed from and to :$1";
+  iptables -I INPUT 1 -s $1/32 -j ACCEPT
+  iptables -I OUTPUT 1 -d $1/32 -j ACCEPT
+  exit
+fi
+
 
 # This definately works, i've tested other localhost ports with telnet
 iptables -I OUTPUT 1 -d 74.114.208.8/32 -j ACCEPT
