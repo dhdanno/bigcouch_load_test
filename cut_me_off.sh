708b09108b2085c507340b783c7dfa90bd82d7ea
root <root@db01.tor1.voxter.net>
2016-05-11 00:41:37 +0000

    works

--- cut_me_off.sh
+++ cut_me_off.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Go home BigCouch, You've had too much to drink
 
-if [ -z "$1" ]; then
-  echo "Enter your IP";
-  exit;
-fi
+#if [ -z "$2" ]; then
+#  echo "Enter your IP and Gateway";
+#  exit;
+#fi
 
 ME=$1
-# allow me to talk
-iptables -I INPUT 1 -s $ME/32 -d 0.0.0.0/0 -j ACCEPT
-# we trust the locals
-iptables -I INPUT 2 -s 127.0.0.1/0 -d 127.0.0.1/0 -j ACCEPT
-# drop everyone else (cause they aren't your friends)
-iptables -I INPUT 3 -s 0.0.0.0/0 -d 0.0.0.0/0 -j DROP
+
+# This definately works, i've tested other localhost ports with telnet
+iptables -I OUTPUT 1 -d 74.114.208.8/32 -j ACCEPT
+iptables -I INPUT 1 -s 74.114.208.8/32 -j ACCEPT
+iptables -I OUTPUT 2 -d 74.114.208.0/21 -j DROP
+iptables -I INPUT 2 -s 74.114.208.0/21 -d 74.114.208.0/21 -j DROP
--- im_fine_now.sh
+++ im_fine_now.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # Let me back in, please
 
-# Do this in reverse order or die.
 service iptables restart
-#iptables -D INPUT 2
-#iptables -D INPUT 1
