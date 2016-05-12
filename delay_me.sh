5840b820b79877de06c666098594da740f2eaedb
root <root@db01.tor1.voxter.net>
2016-05-12 23:08:17 +0000

    changed delay

--- delay_me.sh
+++ delay_me.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 IP=$1
-DELAY="100ms"
-VAR="50ms"
+DELAY="200ms"
+VAR="10ms"
 
 tc qdisc add dev eth0 root netem delay $DELAY
-tc qdisc change dev eth0 root netem delay $DELAY $VAR loss 10%
+tc qdisc change dev eth0 root netem delay $DELAY $VAR loss 30%
 
 #tc qdisc change dev eth0 root netem loss 25%
 
