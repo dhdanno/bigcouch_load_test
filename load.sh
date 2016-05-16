a4b2bd0ead2ea9f221d76b5aa095259b94b88cf3
root <root@db01.tor1.voxter.net>
2016-05-16 22:38:19 +0000

    new db

--- load.sh
+++ load.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 DB="aapoo_base"
-DOC="1bb6a93fe9cd53e53dfa24bf0d00017e"
-HOST="media01.tor1.voxter.net"
+DOC="fce232d2f7d657d17f38b41fbe005b06"
+#HOST="media01.tor1.voxter.net"
 POST="15984"
 
 if [ ! $2 ]; then
@@ -34,7 +34,7 @@ if [ $2 == "PUT" ]; then
      let RAND=RAND+1
      echo $RAND > count
 
-     curl -H "Content-Type:application/json" -X PUT http://127.0.0.1:5984/account%2F00%2Fbd%2F56f2037efc4e8f17b830d9a5cef0/$RAND -d '{"apps":{"userportal":{"api_url":"https://api.test.com:8443/v1","default":true,"desc":"Manageyourcallforwarding,seecallhistoryintheUserPortal","icon":"userportal","id":"userportal","label":"UserPortal"}},"call_forward":{"direct_calls_only":false,"enabled":false,"ignore_early_media":true,"keep_caller_id":true,"number":"","require_keypress":false,"substitute":false},"caller_id":{"external":{"number":"+16NULL"}},"caller_id_options":{},"email":"PUT OPERATION","enabled":true,"fax_to_email_enabled":true,"first_name":"sarahs","hotdesk":{"enabled":false,"keep_logged_in_elsewhere":false,"require_pin":false},"id":"$RAND","is_sub_account_rep":false,"last_name":"b.ca","media":{},"music_on_hold":{},"notifications":{},"priv_level":"user","pvt_account_db":"account%2F00%2F17%2F7c5838d8d2c0a75b31d6389bec72","pvt_account_id":"00177c5838d8d2c0a75b31d6389bec72","pvt_created":63540116038,"pvt_md5_auth":"1dfba8dad69f3c3b101409dd2656a208","pvt_modified":63582351480,"pvt_sha1_auth":"e461b16e109b530797dbbe5103af18c80aa75302","pvt_type":"user","pvt_vsn":"1","require_password_update":false,"timezone":"America/Vancouver","username":"s","verified":false,"vm_to_email_enabled":true}'
+     curl -H "Content-Type:application/json" -X PUT http://127.0.0.1:5984/$DB/$RAND -d '{"apps":{"userportal":{"api_url":"https://api.test.com:8443/v1","default":true,"desc":"Manageyourcallforwarding,seecallhistoryintheUserPortal","icon":"userportal","id":"userportal","label":"UserPortal"}},"call_forward":{"direct_calls_only":false,"enabled":false,"ignore_early_media":true,"keep_caller_id":true,"number":"","require_keypress":false,"substitute":false},"caller_id":{"external":{"number":"+16NULL"}},"caller_id_options":{},"email":"PUT OPERATION","enabled":true,"fax_to_email_enabled":true,"first_name":"sarahs","hotdesk":{"enabled":false,"keep_logged_in_elsewhere":false,"require_pin":false},"id":"$RAND","is_sub_account_rep":false,"last_name":"b.ca","media":{},"music_on_hold":{},"notifications":{},"priv_level":"user","pvt_account_db":"account%2F00%2F17%2F7c5838d8d2c0a75b31d6389bec72","pvt_account_id":"00177c5838d8d2c0a75b31d6389bec72","pvt_created":63540116038,"pvt_md5_auth":"1dfba8dad69f3c3b101409dd2656a208","pvt_modified":63582351480,"pvt_sha1_auth":"e461b16e109b530797dbbe5103af18c80aa75302","pvt_type":"user","pvt_vsn":"1","require_password_update":false,"timezone":"America/Vancouver","username":"s","verified":false,"vm_to_email_enabled":true}'
 
   done
 fi
@@ -44,7 +44,7 @@ if [ $2 == "POST" ]; then
 
   for i in `seq 1 $3`; do
     echo $i
-    curl -H "Content-Type:application/json" -X POST http://127.0.0.1:5984/account%2F00%2Fbd%2F56f2037efc4e8f17b830d9a5cef0/ -d '{"apps":{"userportal":{"api_url":"https://api.test.com:8443/v1","default":true,"desc":"Manageyourcallforwarding,seecallhistoryintheUserPortal","icon":"userportal","id":"userportal","label":"UserPortal"}},"call_forward":{"direct_calls_only":false,"enabled":false,"ignore_early_media":true,"keep_caller_id":true,"number":"","require_keypress":false,"substitute":false},"caller_id":{"external":{"number":"+16NUL"}},"caller_id_options":{},"email":"POST OPERATION","enabled":true,"fax_to_email_enabled":true,"first_name":"sarahs","hotdesk":{"enabled":false,"keep_logged_in_elsewhere":false,"require_pin":false},"id":"$RAND","is_sub_account_rep":false,"last_name":"b.ca","media":{},"music_on_hold":{},"notifications":{},"priv_level":"user","pvt_account_db":"account%2F00%2F17%2F7c5838d8d2c0a75b31d6389bec72","pvt_account_id":"00177c5838d8d2c0a75b31d6389bec72","pvt_created":63540116038,"pvt_md5_auth":"1dfba8dad69f3c3b101409dd2656a208","pvt_modified":63582351480,"pvt_sha1_auth":"e461b16e109b530797dbbe5103af18c80aa75302","pvt_type":"user","pvt_vsn":"1","require_password_update":false,"timezone":"America/Vancouver","username":"s","verified":false,"vm_to_email_enabled":true}'
+    curl -H "Content-Type:application/json" -X POST http://127.0.0.1:5984/$DB/ -d '{"apps":{"userportal":{"api_url":"https://api.test.com:8443/v1","default":true,"desc":"Manageyourcallforwarding,seecallhistoryintheUserPortal","icon":"userportal","id":"userportal","label":"UserPortal"}},"call_forward":{"direct_calls_only":false,"enabled":false,"ignore_early_media":true,"keep_caller_id":true,"number":"","require_keypress":false,"substitute":false},"caller_id":{"external":{"number":"+16NUL"}},"caller_id_options":{},"email":"POST OPERATION","enabled":true,"fax_to_email_enabled":true,"first_name":"sarahs","hotdesk":{"enabled":false,"keep_logged_in_elsewhere":false,"require_pin":false},"id":"$RAND","is_sub_account_rep":false,"last_name":"b.ca","media":{},"music_on_hold":{},"notifications":{},"priv_level":"user","pvt_account_db":"account%2F00%2F17%2F7c5838d8d2c0a75b31d6389bec72","pvt_account_id":"00177c5838d8d2c0a75b31d6389bec72","pvt_created":63540116038,"pvt_md5_auth":"1dfba8dad69f3c3b101409dd2656a208","pvt_modified":63582351480,"pvt_sha1_auth":"e461b16e109b530797dbbe5103af18c80aa75302","pvt_type":"user","pvt_vsn":"1","require_password_update":false,"timezone":"America/Vancouver","username":"s","verified":false,"vm_to_email_enabled":true}'
 
   done
 fi
@@ -56,7 +56,7 @@ if [ $2 == "ATT" ]; then
         echo $i
         RAND=$(echo `date +%s%N` | md5sum | awk '{print $1}')
 
-        curl -H "Content-Type: image/gif" -X PUT 'http://localhost:5984/account%2F00%2Fbb%2F0ae67239d8186941071e80f4e68a/ATTACHME'$RAND'/homer.gif' --data-binary @homer.gif
+        curl -H "Content-Type: image/gif" -X PUT http://localhost:5984/$DB/ATTACHME$RAND/homer.gif --data-binary @homer.gif
 
 
   done
