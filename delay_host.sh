3147b4e3f9c97df5cb5ca69e4d916c259565b20b
root <root@db01.tor1.voxter.net>
2016-05-18 22:37:37 +0000

    fixed

--- delay_host.sh
+++ delay_host.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # Affect traffic to a single host. Delay and packet loss
 
-IP=$1
-DELAY=$2"ms"
-VAR=$3"ms"
-LOSS=$4
-
+IP1=$1
+IP2=$2
+DELAY=$3"ms"
+VAR=$4"ms"
+LOSS=$5
 
 # Purge old policies
 tc qdisc del dev eth0 root
 # Add a filter chain
 tc qdisc add dev eth0 root handle 1: prio
 # Create a new chain for latency
-tc qdisc add dev eth0 parent 1:1 handle 2: netem delay $DELAY
-# Modify it adding loss and variable latency
-tc qdisc change dev eth0 parent 1:1 handle 2: netem delay $DELAY $VAR loss $LOSS%
+tc qdisc add dev eth0 parent 1:3 handle 30: netem delay $DELAY $VAR loss $LOSS%
+
 # Apply to a single host
-tc filter add dev eth0 parent 1:0 protocol ip pref 55 handle ::55 u32 match ip dst $IP flowid 2:1
+tc filter add dev eth0 parent 1:0 protocol ip u32 match ip dst $IP1 flowid 1:3
+tc filter add dev eth0 parent 1:0 protocol ip u32 match ip dst $IP2 flowid 1:3
+
